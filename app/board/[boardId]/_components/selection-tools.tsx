4c96685d48d15ccfb01cfeedb620708f0e4cf8bf
Sayan Mondal <101855586+Sayan-sss@users.noreply.github.com>
2024-03-28 09:09:19 +0530

    added the functionality of changing the layer depth

--- app/board/[boardId]/_components/selection-tools.tsx
+++ app/board/[boardId]/_components/selection-tools.tsx
@@ -1,14 +1,16 @@
 "use client";
 
-import { useSelectionBounds } from "@/hooks/use-selection-bounds";
-import { useMutation, useSelf } from "@/liveblocks.config";
-import { Camera, Color } from "@/types/canvas";
 import { memo } from "react";
-import { ColorPicker } from "./color-picker";
-import { useDeleteLayers } from "@/hooks/use-delete-layers";
+import { BringToFront, SendToBack, Trash2 } from "lucide-react";
+
 import { Hint } from "@/components/hint";
+import { Camera, Color } from "@/types/canvas";
 import { Button } from "@/components/ui/button";
-import { Trash2 } from "lucide-react";
+import { useMutation, useSelf } from "@/liveblocks.config";
+import { useDeleteLayers } from "@/hooks/use-delete-layers";
+import { useSelectionBounds } from "@/hooks/use-selection-bounds";
+
+import { ColorPicker } from "./color-picker";
 
 interface SelectionToolsProps {
   camera: Camera;
@@ -18,6 +20,49 @@ interface SelectionToolsProps {
 export const SelectionTools = memo(
   ({ camera, setLastUsedColor }: SelectionToolsProps) => {
     const selection = useSelf((me) => me.presence.selection);
+
+    const moveToFront = useMutation(
+      ({ storage }) => {
+        const liveLayerIds = storage.get("layerIds");
+        const indices: number[] = [];
+
+        const arr = liveLayerIds.toImmutable();
+
+        for (let i = 0; i < arr.length; i++) {
+          if (selection.includes(arr[i])) {
+            indices.push(i);
+          }
+        }
+
+        for (let i = indices.length - 1; i >= 0; i--) {
+          liveLayerIds.move(
+            indices[i],
+            arr.length - 1 - (indices.length - 1 - i)
+          );
+        }
+      },
+      [selection]
+    );
+
+    const moveToBack = useMutation(
+      ({ storage }) => {
+        const liveLayerIds = storage.get("layerIds");
+        const indices: number[] = [];
+
+        const arr = liveLayerIds.toImmutable();
+
+        for (let i = 0; i < arr.length; i++) {
+          if (selection.includes(arr[i])) {
+            indices.push(i);
+          }
+        }
+
+        for (let i = 0; i < indices.length; i++) {
+          liveLayerIds.move(indices[i], i);
+        }
+      },
+      [selection]
+    );
 
     const setFill = useMutation(
       ({ storage }, fill: Color) => {
@@ -47,12 +92,24 @@ export const SelectionTools = memo(
         className="absolute p-3 rounded-xl bg-white shadow-sm border flex select-none"
         style={{
           transform: `translate(
-        calc(${x}px - 50%),
-        calc(${y - 16}px - 100%)
-      )`,
+          calc(${x}px - 50%),
+          calc(${y - 16}px - 100%)
+        )`,
         }}
       >
         <ColorPicker onChange={setFill} />
+        <div className="flex flex-col gap-y-0.5">
+          <Hint label="Bring to front">
+            <Button onClick={moveToFront} variant="board" size="icon">
+              <BringToFront />
+            </Button>
+          </Hint>
+          <Hint label="Send to back" side="bottom">
+            <Button onClick={moveToBack} variant="board" size="icon">
+              <SendToBack />
+            </Button>
+          </Hint>
+        </div>
         <div className="flex items-center pl-2 ml-2 border-l border-neutral-200">
           <Hint label="Delete">
             <Button variant="board" size="icon" onClick={deleteLayers}>

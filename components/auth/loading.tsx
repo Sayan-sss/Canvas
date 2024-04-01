c356e535f8d57220d7382f7461d22678884a9898
Sayan Mondal <101855586+Sayan-sss@users.noreply.github.com>
2024-04-01 23:19:53 +0530

    deployment bug fixed

--- components/auth/loading.tsx
+++ components/auth/loading.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export const Loading = () => {
   return (
-    <div className=" h-full w-full flex flex-col justify-center items-center">
+    <div className="h-full w-full flex flex-col justify-center items-center">
       <Image
         src="/logo.svg"
-        alt="logo"
+        alt="Logo"
         width={120}
         height={120}
         className="animate-pulse duration-700"
